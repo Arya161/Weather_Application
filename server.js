40de4a6058761285b8b89cac73eaac9258dce6e9
Arya <aryaparolia16@gmail.com>
2024-01-05 10:59:32 +0530

    Third Commit, fixed issues related to undefined variables

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const request = require('request');
+const app = express()
 
 const apiKey = '*****************';
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
-app.get('/', function(req,res){
-    res.render('index')
-});
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
 
-app.post('/',function(req,res){
-    let city = req.body.city;
+app.post('/', function (req, res) {
+  let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-request(url, function (err, response, body) {
+  request(url, function (err, response, body) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
     } else {
@@ -32,7 +32,6 @@ request(url, function (err, response, body) {
   });
 })
 
-app.listen(3000, function(){
-    console.log('Sample listening on port 3000')
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
 })
-
